3d0e201c077edd8795b9c7cb80b83a624a201298
rayed168 <muhammad.rayed2013@gmail.com>
2024-09-23 16:18:11 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,4 @@
+country_code={"Pakistan":"0097","India":"0052","Finland":"0087","Bahrain":"0032"}
+print("Country code for India : ")
+print(country_code.get("India","India is not found"))
+print(country_code.get("Nepal","Nepal is not found"))
